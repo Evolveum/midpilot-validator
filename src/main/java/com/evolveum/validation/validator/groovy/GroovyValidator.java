7f9656f14d3bf9f26aea2ef97b8eba9e88e42649
dominikhorvath <dominikhorvath138@gmail.com>
2026-01-08 16:46:11 +0100

    fix copyright header

--- src/main/java/com/evolveum/validation/validator/groovy/GroovyValidator.java
+++ src/main/java/com/evolveum/validation/validator/groovy/GroovyValidator.java
@@ -1,3 +1,11 @@
+/*
+ *
+ *  * Copyright (c) 2025 Evolveum and contributors
+ *  *
+ *  * Licensed under the EUPL-1.2 or later.
+ *
+ */
+
 package com.evolveum.validation.validator.groovy;
 
 import com.evolveum.concepts.SourceLocation;
